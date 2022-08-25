4098d1f9882ff72979ee9d07c3864415da0ebe9a
Endika Aguirre <55593360+Ninjalice@users.noreply.github.com>
2022-08-25 19:51:07 +0200

    Update comingsoonjs.js

--- static/js/comingsoonjs.js
+++ static/js/comingsoonjs.js
@@ -7,7 +7,7 @@ function like() {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 30, 2022 16:00:00").getTime();
+var countDownDate = new Date("Sep 30, 2022 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
